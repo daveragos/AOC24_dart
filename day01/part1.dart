7ded5b6772bc9b5a8f0c21d96d765fd1b94a146e
RaGoose <daveyeinde@gmail.com>
2024-12-01 22:49:45 +0300

    started day01

---
+++ day01/part1.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+void main() async {
+  final input = await File('day01/input.txt').readAsLines();
+
+  solvePart1(input);
+  solvePart2(input);
+}
+
+void solvePart1(List<String> input) {
+  print('Part 1 result:');
+}
+
+void solvePart2(List<String> input) {
+  print('Part 2 result:');
+}
