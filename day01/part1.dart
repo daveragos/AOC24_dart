8fc0ce4826a5d558eddb87f6e14a5fb6025242ff
RaGoose <daveyeinde@gmail.com>
2024-12-01 23:21:37 +0300

    reading the left and right list off of the input file

--- day01/part1.dart
+++ day01/part1.dart
@@ -1,14 +1,27 @@
 import 'dart:io';
 
 void main() async {
-  final input = await File('day01/input.txt').readAsLines();
+  String currentPath = Directory.current.path;
+  final input = await File('$currentPath/input.txt').readAsLines();
 
-  solvePart1(input);
+  final list = getList(input);
+  print(list[0][0]);
+  print(list[1][0]);
   solvePart2(input);
 }
 
-void solvePart1(List<String> input) {
-  print('Part 1 result:');
+List<List<int>> getList(List<String> input) {
+  List<int> left = [];
+  List<int> right = [];
+
+  for ( int i=0 ; i < input.length ; i++ ){
+    final line = input[i];
+    final numList = line.split('   ');
+    left.add(int.parse(numList[0]));
+    right.add(int.parse(numList[1]));
+
+  }
+  return [left, right];
 }
 
 void solvePart2(List<String> input) {
